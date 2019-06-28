5e751e98419c779a33edb529d017bcebc106b231
Kabir Karamchandani <kabirkaramchandani@gmail.com>
2019-06-28 17:38:31 -0400

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const hbs= require('hbs')
 
 const app= express()
+const port= process.env.PORT || 3000
 
 //set up handlebars engine
 app.set('view engine', 'hbs')
@@ -80,6 +81,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000, ()=>{
-    console.log('Server is up on 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on '+port)
 })
